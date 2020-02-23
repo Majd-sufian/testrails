d3449d9a63332ee64df8742459e6b799081b8edc
Majd Sufian <majdnewsufian@gmail.com>
2020-02-23 16:50:42 +0100

    add a new column to the migration

---
+++ db/migrate/20200223154336_add_test_to_tests.rb
@@ -0,0 +1,4 @@
+class AddTestToTests < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_154011) do
+ActiveRecord::Schema.define(version: 2020_02_23_154644) do
 
   create_table "tests", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "done"
   end
 
 end
