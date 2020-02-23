a8fec0585e34146031223066636a1ad972f47f87
Majd Sufian <majdnewsufian@gmail.com>
2020-02-23 16:41:41 +0100

    adding a test model

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,15 @@
+class TestsController < ApplicationController
+  RESTAURANTS = {
+    1 => { name: "Dishoom", address: "Shoreditch, London", category: "indian" },
+    2 => { name: "Sushi Samba", address: "City, London", category: "japanese" }
+  }
+
+  def index
+    @restaurants = RESTAURANTS
+  end
+
+  def selectCategory
+    @category = params[:id]
+    @restaurantCategory = RESTAURANTS.select { |id, r| r[:category] == @category}
+  end
+end
